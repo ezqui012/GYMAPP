cf09410e96ce4f4bdcde7febbd1a2c98eab7cd38
Ezequiel Rene Lafuente <ezqui016@gmail.com>
2025-11-25 19:23:44 -0400

    initial commit

---
+++ frontend/controllers/clients/editClient.js
@@ -0,0 +1,280 @@
+import { loadComponent } from "../../app/app.js";
+export function initEditClient() {
+  const btnSubmit = document.getElementById("submit");
+  const btnCancelSubmit = document.querySelector('.btn_cancel');
+  const allInput = document.querySelectorAll(".field_data");
+  const btnSubmitModal = document.getElementById("modal_submit");
+  const btnCloseModal = document.getElementById("close_modal");
+  const toastContainer=document.querySelector('.toast_container');
+  const urlParams = new URLSearchParams(window.location.search);
+  const userId = parseInt(urlParams.get("id"));
+
+
+  function getUserData() {
+    let userFound=false;
+    let userList = JSON.parse(localStorage.getItem("usersList"));
+    for (let i = 0; i < userList.length; i++) {
+      if (userId === userList[i].id) {
+        const fieldName = document.getElementById("name");
+        fieldName.value = userList[i].name;
+        const fieldLastName = document.getElementById("lastName");
+        fieldLastName.value = userList[i].lastName;
+        const fieldPhone = document.getElementById("phone");
+        fieldPhone.value = userList[i].phone;
+        const fieldCi = document.getElementById("ci");
+        fieldCi.value = userList[i].ci;
+        const fieldNit = document.getElementById("nit");
+        fieldNit.value = userList[i].nit;
+        const fieldEmail = document.getElementById("email");
+        fieldEmail.value = userList[i].email;
+        const fieldPhoto = document.getElementById("photo");
+        fieldPhoto.value = "photo";
+        userFound=true;
+        break;
+      } 
+    }
+    if(!userFound){
+      console.log(`usuario no encontrado`);
+    }
+  }
+  getUserData(); 
+
+  const showToast = (checkform) => {
+    let message = "";
+    let option = "";
+    if (checkform) {
+      message = "Se modificaron los datos con éxito!!";
+      option = "sucess";
+    } else {
+      message = "Hubo un error al guardar, intenta de nuevo";
+      option = "error";
+    }
+    let toastNotification = `<div class="toast ${option}">
+                            <p class="toast_message">${message}</p>
+                            </div>`;
+    return toastNotification;
+  };
+  const removeToast = () => {
+    toastContainer.removeChild(toastContainer.firstChild);
+  };
+
+  const updateUserProfile = (id) => {
+    let usersList = JSON.parse(localStorage.getItem("usersList")) || [];
+
+    for (let i = 0; i < usersList.length; i++) {
+      if (userId === usersList[i].id) {
+        const alertDialog = document.getElementById("alert-dialog");
+        let checkForm = alertDialog.dataset.checkForm;
+        if (checkForm) {
+          const toastNotification = showToast(checkForm);
+          toastContainer.innerHTML = toastNotification;
+          usersList[i].name = document.getElementById("name").value;
+          usersList[i].lastName = document.getElementById("lastName").value;
+          usersList[i].phone = document.getElementById("phone").value;
+          usersList[i].ci = document.getElementById("ci").value;
+          usersList[i].nit = document.getElementById("nit").value;
+          usersList[i].photo = document.getElementById("photo").value;
+          usersList[i].email = document.getElementById("email").value;
+          localStorage.setItem("usersList", JSON.stringify(usersList));
+          alertDialog.close();
+          setTimeout(() => {
+            removeToast();
+          }, 3000);
+          break;
+        } else {
+          const toastNotification = showToast(checkForm);
+          toastContainer.innerHTML = toastNotification;
+          
+          break;
+        }
+      }
+    }
+  };
+
+  const validateField = (field, id) => {
+    let isValid = false;
+    const inputError = document.getElementById(`${id}_error`);
+    let clearField = field.value.trim();
+    if (id === "name") {
+      if (clearField && clearField.length >= 4 && clearField.length <= 18) {
+        field.classList.add("valid");
+        field.classList.remove("error");
+        inputError.classList.remove("show");
+        isValid = true;
+      } else if (clearField === "") {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent = "El campo se encuentra vacío";
+        isValid = false;
+      } else {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent =
+          "El campo debe tener mas de 4 y menos de 18 palabras";
+        isValid = false;
+      }
+    }
+    if (id === "lastName") {
+      if (clearField && clearField.length >= 4 && clearField.length <= 18) {
+        field.classList.add("valid");
+        field.classList.remove("error");
+        inputError.classList.remove("show");
+        isValid = true;
+      } else if (clearField === "") {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent = "El campo se encuentra vacío";
+        isValid = false;
+      } else {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent =
+          "El campo debe tener mas de 4 y menos de 18 palabras";
+        isValid = false;
+      }
+    }
+    if (id === "phone") {
+      if (clearField && clearField.length >= 7 && clearField.length <= 18) {
+        field.classList.add("valid");
+        field.classList.remove("error");
+        inputError.classList.remove("show");
+        isValid = true;
+      } else if (clearField === "") {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent = "El campo se encuentra vacío";
+        isValid = false;
+      } else {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent =
+          "El campo debe tener mas de 7 dígitos y menos de 10 dígitos";
+        isValid = false;
+      }
+    }
+    if (id === "ci") {
+      if (clearField && clearField.length >= 7 && clearField.length <= 12) {
+        field.classList.add("valid");
+        field.classList.remove("error");
+        inputError.classList.remove("show");
+        isValid = true;
+      } else if (clearField === "") {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent = "El campo se encuentra vacío";
+        isValid = false;
+      } else {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent =
+          "El campo debe tener mas de 7 dígitos y menos de 12 dígitos";
+        isValid = false;
+      }
+    }
+    if (id === "nit") {
+      if (clearField && clearField.length >= 1 && clearField.length <= 10) {
+        field.classList.add("valid");
+        field.classList.remove("error");
+        inputError.classList.remove("show");
+        isValid = true;
+      } else if (clearField === "") {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent = "El campo se encuentra vacío";
+        isValid = false;
+      } else {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent =
+          "El campo debe tener mas de 1 dígitos y menos de 10 dígitos";
+        isValid = false;
+      }
+    }
+    if (id === "photo") {
+      if (clearField) {
+        field.classList.add("valid");
+        field.classList.remove("error");
+        inputError.classList.remove("show");
+        isValid = true;
+      } else if (clearField === "") {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent = "Debe ingresar una foto de perfil";
+        isValid = false;
+      } else {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        isValid = false;
+      }
+    }
+    if (id === "email") {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (clearField && emailRegex.test(clearField)) {
+        field.classList.add("valid");
+        field.classList.remove("error");
+        inputError.classList.remove("show");
+        isValid = true;
+      } else if (clearField === "") {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent = "Debe ingresar un correo vàlido";
+        isValid = false;
+      } else {
+        field.classList.add("error");
+        field.classList.remove("valid");
+        inputError.classList.add("show");
+        inputError.textContent = "Debe ingresar un correo vàlido";
+        isValid = false;
+      }
+    }
+    return isValid;
+  };
+
+  btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const alertDialog = document.getElementById("alert-dialog");
+    console.log("llega");
+    let checkForm = true;
+    let checks = [];
+    allInput.forEach((oneInput) => {
+      const isValid = validateField(oneInput, oneInput.id);
+      if (!isValid) {
+        checks.push(false);
+      } else checks.push(true);
+    });
+    checkForm = checks.every((check) => check === true);
+    if (checkForm) {
+      alertDialog.dataset.checkForm = checkForm;
+      alertDialog.show();
+    }
+    
+  });
+
+  btnCancelSubmit.addEventListener('click',()=>{
+    window.history.pushState({}, "", "/clientList");
+    loadComponent();
+  })
+  btnSubmitModal.addEventListener("click", (e) => {
+      e.preventDefault();
+      updateUserProfile(userId);
+    });
+
+  btnCloseModal.addEventListener("click", () => {
+    const alertDialog = document.getElementById("alert-dialog");
+    alertDialog.close();
+  });
+  
+}
---
+++ frontend/controllers/membership/membershipList.js
@@ -0,0 +1,219 @@
+import { loadComponent } from "../../app/app.js";
+export function initMembershipList(){
+  const btnAddMem = document.querySelector(".add_membership");
+  const searchBar = document.getElementById("searchbar");
+  const containerBtn = document.querySelector(".btn_numbers");
+  const btnForward = document.querySelector(".forward_btn");
+  const btnBack = document.querySelector(".back_btn");
+  
+  let since = 0;
+  let limit = 11;
+  let activePage = 1;
+  let membershipList = getMembershipList();
+  
+  let pageNumber = Math.ceil(membershipList.length / limit);
+  
+  let editButton = () => {
+    const editButton = document.querySelectorAll(".edit_data");
+    editButton.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        let id = e.currentTarget.dataset.index;
+        window.history.pushState({}, "", `/editMembership?id=${id}`);
+        loadComponent();
+      });
+    });
+  };
+
+  let deleteAction = () => {
+    const deleteMembership = document.querySelectorAll(".delete_data");
+    deleteMembership.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        let id = e.currentTarget.dataset.index;
+        deleteData(id);
+        loadList();
+      });
+    });
+  };
+
+  let deleteData = (id) => {
+    if (id !== null) {
+      let membership = membershipList.filter((memberships) => memberships._idMembership !== parseInt(id));
+      membershipList=membership
+      localStorage.setItem("membershipList", JSON.stringify(membershipList));
+      loadList();
+    } else {
+      console.log("error con la posicion del elemento" + pos);
+    }
+  };
+    
+    function getUserData(id){
+      const userslist= JSON.parse(localStorage.getItem('usersList'))||[];
+      const userData=userslist.find((user)=>user.id===parseInt(id));
+      return userData;
+    }
+    function getMembershipList(){
+        const membershipList = JSON.parse(localStorage.getItem('membershipList'));
+        return membershipList;
+    }
+
+    function getMembershipTypeList(){
+      const membershipTypeList = JSON.parse(localStorage.getItem('membershipTypeList'))||[];
+      return membershipTypeList;
+    }
+
+    const getAMembershipType=(id)=>{
+      const membershipTypeList=getMembershipTypeList();
+      let membershipTypeData=membershipTypeList.find(membershipType=>membershipType.id===id);
+      return membershipTypeData;
+    } 
+
+
+    const displayButtonOption = (id) => {
+    let buttons = `<button class='btn_action view_data' data-index='${id}'><i class="fas fa-eye ver-btn" title="Ver"></i></button>
+                 <button class='btn_action edit_data' data-index='${id}'><i class="fas fa-pen editar-btn" title="Editar"></i></button>
+                 <button class='btn_action delete_data' data-index='${id}' ><i class="fas fa-trash borrar-btn" title="Borrar"></i></button>`;
+    return buttons;
+  };
+
+    const loadList = () => {
+    
+    const tbodyContainer = document.querySelector(".tbody_container");
+    tbodyContainer.innerHTML = "";
+
+    for (let i = since; i < since + limit && i < membershipList.length; i++) {
+      const trContainer = document.createElement("tr");
+      trContainer.classList.add("data-row");
+
+      const tdClient = document.createElement("td");
+      tdClient.classList.add("data-table");
+      const clientData=getUserData(membershipList[i]._idClient);
+      tdClient.innerHTML = `${clientData.name} ${clientData.lastName}` ;
+      trContainer.appendChild(tdClient);
+
+      const tdMembership = document.createElement("td");
+      tdMembership.classList.add("data-table");
+      const idMembershipType = membershipList[i]._idMembershipType;
+      tdMembership.innerHTML = getAMembershipType(idMembershipType)._membershipName;
+      trContainer.appendChild(tdMembership);
+
+      const tdInitDate = document.createElement("td");
+      tdInitDate.classList.add("data-table");
+      tdInitDate.innerHTML = membershipList[i]._initDate;
+      trContainer.appendChild(tdInitDate);
+
+      const tdEndDate = document.createElement("td");
+      tdEndDate.classList.add("data-table");
+      tdEndDate.innerHTML = membershipList[i]._endDate;
+      trContainer.appendChild(tdEndDate);
+
+      let tdButtons = document.createElement("td");
+      tdButtons.classList.add("actions");
+      tdButtons.innerHTML = displayButtonOption(membershipList[i]._idMembership);
+      trContainer.appendChild(tdButtons);
+      tbodyContainer.appendChild(trContainer);
+    }
+    editButton();
+    deleteAction();
+    loadButtonPage();
+    changePageListeners();
+  };
+  loadList();
+  function loadButtonPage() {
+    containerBtn.innerHTML = "";
+    if (pageNumber === 0) {
+      let buttonChange = document.createElement("button");
+      buttonChange.classList.add("btn_page");
+      buttonChange.setAttribute("id", 1);
+      buttonChange.innerHTML = 1;
+      containerBtn.append(buttonChange);
+    } else {
+      for (let i = 0; i < pageNumber; i++) {
+        let buttonChange = document.createElement("button");
+        buttonChange.classList.add("btn_page");
+        buttonChange.setAttribute("id", i + 1);
+        buttonChange.innerHTML = i + 1;
+        containerBtn.append(buttonChange);
+      }
+    }
+  }
+  function changePageBack() {
+    if (activePage > 1) {
+      limit = since;
+      since = since - 11;
+      activePage--;
+      membership.slice(since, limit);
+      loadList();
+    }
+  }
+  function changePageForward() {
+    if (activePage < pageNumber) {
+      since = limit;
+      limit = limit + 11;
+      activePage++;
+      membership.slice(since, limit);
+      loadList();
+    }
+  }
+
+  function changePageNumber(page) {
+    if (activePage < page) {
+      activePage = page;
+      limit = 11 * page;
+      since = limit - 11;
+      membership.slice(since, limit);
+      loadList();
+    } else if (activePage > page) {
+      let pageDifference = activePage - page;
+      since = since - pageDifference * 11;
+      limit = limit - pageDifference * 11;
+      activePage = page;
+      membership.slice(since, limit);
+      loadList();
+    }
+  }
+
+  function changePageListeners() {
+    let pageButtons = document.querySelectorAll(".btn_page");
+    for (let i = 0; i < pageButtons.length; i++) {
+      pageButtons[i].addEventListener("click", (e) => {
+        e.preventDefault();
+        changePageNumber(parseInt(pageButtons[i].id));
+      });
+    }
+  }
+
+  searchBar.addEventListener("keyup", () => {
+    let mainTrContainer = document.querySelectorAll(".data-row");
+    const keyword = searchBar.value.toLowerCase().trim();
+
+    for (let i = 0; i < mainTrContainer.length; i++) {
+      const tdList = mainTrContainer[i].getElementsByTagName("td");
+      let wordIsFound = false;
+      for (let j = 0; j < tdList.length; j++) {
+        let wordToCompare = tdList[j].textContent.toLowerCase().trim();
+        if (wordToCompare.includes(keyword)) {
+          wordIsFound = true;
+          break;
+        }
+      }
+      mainTrContainer[i].style.display = wordIsFound ? "" : "none";
+    }
+  });
+
+  btnAddMem.addEventListener("click", () => {
+    window.history.pushState({}, "", "/membership");
+    loadComponent();
+  });
+  btnForward.addEventListener("click", (e) => {
+    e.preventDefault();
+    changePageForward();
+  });
+
+  btnBack.addEventListener("click", (e) => {
+    e.preventDefault();
+    changePageBack();
+  });
+  
+}
+initMembershipList();
