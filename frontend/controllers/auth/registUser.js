f572884f965f85f596bd06034e56428f3034c915
ezeLaf <ezqui016@gmail.com>
2026-04-02 23:39:25 -0400

    feat(frontend): registUser.html form updated, styles updated

--- frontend/controllers/auth/registUser.js
+++ frontend/controllers/auth/registUser.js
@@ -4,6 +4,39 @@ import { loadComponent } from "../../app/app.js";
 export async function initRegistUser() {
     
     const btnInitLog= document.querySelector('.init_log');
+    const btnSubmit= document.querySelector('.submit_button');
+
+    const registUser=async()=>{
+        try {
+            const email=document.getElementById('email').value;
+            const pass=document.getElementById('pass').value;
+            const response = await fetch('http://localhost:8500/registUser',{
+                method: 'POST',
+                headers:{'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    password: pass,
+                    email: email
+                    
+                })
+            })
+            if(!response.ok)throw new Error("error processing data");
+
+            const data=await response.json();
+            console.log(data);
+            console.log('Usuario creado correctamente');
+        }
+        catch (error) {
+            console.error("error en la peticion", error)
+        } 
+    }
+        
+        
+
+    btnSubmit.addEventListener('click', (e)=>{
+        e.preventDefault();
+        registUser();
+    })
+
 
     btnInitLog.addEventListener("click",(e)=>{
         e.preventDefault();
