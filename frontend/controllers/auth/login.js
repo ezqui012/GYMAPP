4aac9a9f3e7ad056ac8267d5f4e1d3ba02cf8160
ezeLaf <ezqui016@gmail.com>
2026-06-05 19:44:06 -0400

    feat(frontend) login service was implemented

--- frontend/controllers/auth/login.js
+++ frontend/controllers/auth/login.js
@@ -1,5 +1,5 @@
 import { loadComponent } from "../../app/app.js";
-
+const API_URL="http://localhost:8500";
 
 export async function initLogin(){
     let users=[
@@ -18,10 +18,49 @@ export async function initLogin(){
         "name":"maria" ,
         "password":"12345678" 
     },
-]
-let btnLog=document.querySelector('.submit_button');
-const btnAdd=document.querySelector('.btn_add_user');
-let verifyAccount=(user,password)=>{
+    ]
+
+    const loginService=async(email, password)=>{
+        try {
+            const response = await fetch(`${API_URL}/logUser`, {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({email, password}),
+                credentials: "include"
+            })
+
+            if(!response.ok) throw new Error('Error processing data');
+
+            const userData=response.json();
+
+            return userData;
+
+        } catch (error) {
+            console.log('Logging error')
+        }
+    }
+    
+    const login=async()=>{
+        let userData=document.getElementById('user').value;
+        let userPass=document.getElementById('pass').value;
+        const isLogged= await loginService(userData, userPass);
+        if(isLogged){
+            console.log('redirigiendo....')
+            setTimeout(() => {
+
+                window.location.href='http://localhost:3000/';
+            }, 1000);
+            
+        }else{
+            alert("usuario invalido")
+        }
+    }
+
+    let btnLog=document.querySelector('.submit_button');
+    const btnAdd=document.querySelector('.btn_add_user');
+    let verifyAccount=(user,password)=>{
     for (let i = 0; i < users.length; i++) {
         if(user===users[i].user && password===users[i].password){
             return true; 
@@ -29,34 +68,24 @@ let verifyAccount=(user,password)=>{
     }
     return false;
     
-}
-
-btnAdd.addEventListener('click', (e)=>{
-    e.preventDefault();
-    let route = btnAdd.dataset.route;
-    
-    if(route){
-        window.history.pushState({}, "", route);
-        loadComponent();  
     }
-})
 
-btnLog.addEventListener("click", (e)=>{
-    e.preventDefault()
-    let userData=document.getElementById('user').value;
-    let userPass=document.getElementById('pass').value
-    if(verifyAccount(userData,userPass)){
-        console.log('redirigiendo....')
-        setTimeout(() => {
-
-            window.location.href='app/app.html';
-        }, 1000);
+    btnAdd.addEventListener('click', (e)=>{
+        e.preventDefault();
+        let route = btnAdd.dataset.route;
         
-    }else{
-        alert("usuario invalido")
-    }
-   
-});
+        if(route){
+            window.history.pushState({}, "", route);
+            loadComponent();  
+        }
+    })
+
+    btnLog.addEventListener("click", (e)=>{
+        e.preventDefault()
+        login();
+        
+    
+    });
 }
 
 
